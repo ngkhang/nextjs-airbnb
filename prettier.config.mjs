55de9374fb0a7a80ec0795ebc0f5323d4450b71e
Khang Nguyen <ngkhang0220@gmail.com>
2024-10-16 22:48:56 +0700

    setup: implement ESLint and Prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'crlf',
+  jsxSingleQuote: false,
+};
+
+export default config;
