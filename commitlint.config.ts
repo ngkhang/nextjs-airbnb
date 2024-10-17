87c024ce4a2c520f94a1b41a3ad691f5e2883ba8
Khang Nguyen <ngkhang0220@gmail.com>
2024-10-17 21:07:20 +0700

    build(setup): add code quality tools and git hooks
    add tools: Husky, Lint-staged, Commitlint, Commitizen

---
+++ commitlint.config.ts
@@ -0,0 +1,53 @@
+import type { UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+  prompt: {
+    questions: {
+      scope: {
+        description:
+          'What is the scope of this change (e.g. component or file name)',
+      },
+      subject: {
+        description:
+          'Write a short, imperative tense description of the change and end of ref issue (if have)',
+      },
+      body: {
+        description: 'Provide a longer description of the change',
+      },
+      isIssueAffected: {
+        description: 'Does this change affect any open issues?',
+      },
+      issuesBody: {
+        description:
+          'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+      },
+      issues: {
+        description: 'Add issue references (e.g. "Fix: #123", "Ref: #123".)',
+      },
+    },
+  },
+};
+
+export default Configuration;
---
+++ lint-staged.config.mjs
@@ -0,0 +1,13 @@
+import path from 'path';
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const config = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
+  '*.{json,md}': ['prettier --write'],
+}
+
+export default config;
