54cb0fb3cd00af2db2f59a519ded20957ca12cac
Khang Nguyen <ngkhang0220@gmail.com>
2024-11-18 00:18:30 +0700

    feat: add static content and config image in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    localPatterns: [
+      {
+        pathname: '/assets/**',
+        search: ''
+      }
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'airbnbnew.cybersoft.edu.vn',
+        pathname: '*'
+      },
+    ]
+  }
+};
 
 export default nextConfig;
