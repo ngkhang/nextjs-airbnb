c0a537c24d3d5909b5d7cba11e645d9d35d2518b
Khang Nguyen <ngkhang0220@gmail.com>
2024-12-05 14:25:45 +0700

    feat(auth): implement register functionality

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import httpClient from '@/lib/axios.config';
 import { KEYS } from '@/utils/constants/env';
-import { LoginResponse, LogInType } from '@/types/auth.type';
+import { LoginResponse, LogInType, RegisterResponse, RegisterType } from '@/types/auth.type';
 import { ErrorResponse } from '@/types/common.type';
 import API from '@/utils/constants/api';
 import cookieClient from '@/utils/cookiesClient';
@@ -15,6 +15,16 @@ const authService = {
       return data;
     } catch (error) {
       console.log('🚀 ~ login: ~ error:', error);
+      throw error;
+    }
+  },
+
+  register: async (credentials: RegisterType): Promise<RegisterResponse | ErrorResponse> => {
+    try {
+      const { data } = await httpClient.post<RegisterResponse>(API.AUTH.REGISTER, credentials);
+      return data;
+    } catch (error) {
+      console.log('🚀 ~ register: ~ error:', error);
       throw error;
     }
   },
